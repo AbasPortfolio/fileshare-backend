6a5dd43ea5df301a5366febbc29203bd07b50a4d
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-05-15 14:32:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://file-z7t3.onrender.com"
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', router);
